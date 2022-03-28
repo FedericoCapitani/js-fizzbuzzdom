46bb781bc8534cb7ecd7bc014a0ba3facf70a454
FedericoCapitani <federico.capitani@outlook.it>
2022-03-28 14:57:03 +0200

    creare console log per controllo del programma

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,24 +9,29 @@ for(i = 1; i <= 100; i++){
         number.append(`Fizz`)
         numberList.append(number)
         number.classList.add('square', 'bg_fizz')
+        console.log('fizz');
     } else if (i % 3 != 0 && i % 5 == 0){
         // sostituire elementi divisibile per 3 con buzz
         let number = document.createElement('span')
         number.append(`Buzz`)
         numberList.append(number)
         number.classList.add('square', 'bg_buzz')
+        console.log('buzz');
     } else if (i % 3 == 0 && i % 5 == 0){
         // sostituire elementi divisibile per 3 e per 5 con fizzbuzz
         let number = document.createElement('span')
         number.append(`FizzBuzz`)
         numberList.append(number)
         number.classList.add('square', 'bg_fizzbuzz')
+        console.log('fizzbuzz')
     } else {
         // scrivere i restanti numeri
         let number = document.createElement('span')
         number.append(`${i}`)
         numberList.append(number)
         number.classList.add('square', 'bg_number')
+        console.log(i);
     }
-    console.log(i)
+    // controllo per la stampa completa dei numeri
+    // console.log(i)
 }
